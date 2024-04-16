3e5b04460922ed084a6864951d1a36d4b7cec214
Hyunjin Choi <chg5638@gmail.com>
2024-04-16 17:12:25 +0900

    leveldesign processing

--- Assets/Undead Survivor/Scripts/GameManager.cs
+++ Assets/Undead Survivor/Scripts/GameManager.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public float gameTime;
+    public float maxgameTime = 2 * 10f;
+
     public PoolManager pool;
     public Player player;
 
     void Awake()
     {
         instance = this;
+    }
+
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxgameTime)
+        {
+            gameTime = maxgameTime;
+        }
     }
 }
--- Assets/Undead Survivor/Scripts/Spawner.cs
+++ Assets/Undead Survivor/Scripts/Spawner.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
 
+    int level;
     float timer;
+
 
     void Awake()
     {
@@ -16,8 +19,9 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f); 
 
-        if (timer > 0.2f)
+        if (timer > (level == 0 ? 0.5f : 0.2f))
         {
             timer = 0;
             Spawn();
@@ -26,7 +30,18 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+        GameObject enemy = GameManager.instance.pool.Get(level);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
     }
 }
+
+
+[System.Serializable]
+public class SpawnData
+{
+    public int spriteType;
+    public float spawnTime;
+    public int helath;
+    public float speed;
+}
+
